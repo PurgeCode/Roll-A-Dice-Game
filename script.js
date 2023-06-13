7155ea641fe3c8428d40a2d365a15fadda187d31
Jury <jury.romblondev@gmail.com>
2023-06-13 11:25:59 +0800

    Fixed Images

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Selecting Elements on the HTML
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -10,6 +12,7 @@ const btnHold = document.querySelector('.btn--hold');
 score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -18,5 +21,12 @@ btnRoll.addEventListener('click', function () {
 
   //Displaying the Dice
   diceEl.classList.remove('hidden');
-  diceEl.src = `/img/dice-${dice}.png`;
+  diceEl.src = `./img/dice-${dice}.png`;
+
+  //Checking if the rolled dice is 1
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //Change later
+  }
 });
